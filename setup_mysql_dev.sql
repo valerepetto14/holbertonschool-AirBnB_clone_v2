ad467dbc64abdfce0ba7d4c75b6128c09f6a9153
agusfl <agusflom@hotmail.com>
2022-08-07 20:06:38 +0000

    task 3 - preparing MySQL server for development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,18 @@
+-- Write a script that prepares a MySQL server for the project:
+-- *A database hbnb_dev_db
+-- *A new user hbnb_dev (in localhost)
+-- *The password of hbnb_dev should be set to hbnb_dev_pwd
+-- *hbnb_dev should have all privileges on the database hbnb_dev_db (and only this database)
+-- *hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
+-- *If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
+-- info: https://www.digitalocean.com/community/tutorials/how-to-create-a-new-user-and-grant-permissions-in-mysql
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
 USE hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-FLUSH PRIVILEGES; 
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION; 
-GRANT SELECT ON *.* TO 'performance_schema'@'localhost' WITH GRANT OPTION; 
-FLUSH PRIVILEGES; 
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
