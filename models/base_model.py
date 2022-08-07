026d823988a046ff2b252d809f18308c53cd6fef
agusfl <agusflom@hotmail.com>
2022-08-07 19:37:09 +0000

    change in  __str__ method

--- models/base_model.py
+++ models/base_model.py
@@ -24,8 +24,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
