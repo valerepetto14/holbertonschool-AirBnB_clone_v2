c92c8f14479bb77032d22239be1a01029ad38339
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-08 01:17:02 +0000

    changes in file storage method delete

---
+++ test.delete.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Test delete feature
+"""
+from models.engine.file_storage import FileStorage
+from models.state import State
+
+fs = FileStorage()
+
+new_state = State()
+new_state.name = "California"
+fs.new(new_state)
+fs.save()
+print("New State: {}".format(new_state))
+
+all_states = fs.all(State)
+print("All States: {}".format(len(all_states.keys())))
+for state_key in all_states.keys():
+    print(all_states[state_key])
+
+fs.delete(new_state)
