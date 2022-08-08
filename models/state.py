f45cb255856599136adaa20d8a67f70353718337
agusfl <agusflom@hotmail.com>
2022-08-08 23:43:43 +0000

    adding condition for type of storage and getter (@property) for FileStorage

--- models/state.py
+++ models/state.py
@@ -2,7 +2,11 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, String
+from models.city import City
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -10,6 +14,28 @@ class State(BaseModel, Base):
     # creamos la tabla "states" la clase City va a estar asociada a esta tabla
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    # Relacionamos State con City - si se elimina un Estado, se tendrian que
-    # eliminar todas las ciudades dentro de ese Estado.
-    cities = relationship('City', backref="state")
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        # Relacionamos State con City - si se elimina un Estado, se tendrian
+        # que eliminar todas las ciudades dentro de ese Estado.
+        cities = relationship('City', backref="state", cascade="all ,\
+                                delete-orphan")
+    else:
+        # Si se llega a este else es porque se esata en el storage: FileStorage
+        # Seteamos un getter (usando el decorador: @property)
+        @property
+        def cities(self):
+            """
+            Getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id => It will be the
+            FileStorage relationship between State and City.
+            """
+            return_list = []
+            # Guardamos todas las ciudades que haya en la clase City
+            cities = models.storage.all(City).values()
+            for city in cities:
+                # Se compara id del objeto de tipo State con City
+                if self.id == city.state_id:
+                    # Se guarda el resultado en la lista a retornar
+                    return_list.append(models.storage.all(City)[city])
+            return return_list
