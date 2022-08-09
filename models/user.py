b162a5b1491eb0965f5e4952e5790cad659f2818
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-09 05:42:32 +0000

    fix user

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ this model define class user """
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from models.city import City
 from sqlalchemy.orm import relationship
