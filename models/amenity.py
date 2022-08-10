3b25386cc2a855f2f3dc92f4ae20ea6182210a65
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-10 04:42:32 +0000

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"""Amenity class for AirBnb project"""
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.place import place_amenity
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Amenity class that creates amenities table"""
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity,
-                                   viewonly=True)
+class Amenity(BaseModel):
+    name = ""
--- models/place.py
+++ models/place.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from models.review import Review
 from os import getenv
-
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60), ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False))
-
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -33,45 +25,22 @@ class Place(BaseModel, Base):
         # que eliminar todas las ciudades dentro de ese Estado.
         reviews = relationship('Review', backref="place", cascade="all ,\
                                 delete-orphan")
-        amenities = relationship('Amenity', backref="places",
-                                 secondary=place_amenity,
-                                 viewonly=False)
     else:
         # Si se llega a este else es porque se esata en el storage: FileStorage
         # Seteamos un getter (usando el decorador: @property)
         @property
         def reviews(self):
             """
-            Getter attribute reviews that returns the list of Review instances
-            with place_id equals to the current Place.id => It will be the
-            FileStorage relationship between Place and Review
+            Getter attribute cities that returns the list of City instances
+            with state_id equals to the current State.id => It will be the
+            FileStorage relationship between State and City.
             """
             return_list = []
-            # Guardamos todas las ciudades que haya en la clase Review
+            # Guardamos todas las ciudades que haya en la clase City
             reviews = models.storage.all(Review).values()
             for review in reviews:
-                # Se compara id del objeto de tipo Place con reviews
+                # Se compara id del objeto de tipo State con City
                 if self.id == review.place_id:
                     # Se guarda el resultado en la lista a retornar
                     return_list.append(models.storage.all(Review)[review])
             return return_list
-
-        @property
-        def amenities(self):
-            """
-            comments
-            """
-            return_list = []
-            # Guardamos todas las ciudades que haya en la clase Review
-            amenities = models.storage.all(Amenity).values()
-            for amenity in amenities:
-                # Se compara id del objeto de tipo Place con reviews
-                if self.id == amenity.place_id:
-                    # Se guarda el resultado en la lista a retornar
-                    return_list.append(models.storage.all(Amenity)[amenity])
-            return return_list
-
-        @amenities.setter
-        def amenities(self, amenity_obj):
-            if amenity_obj.__clas__.__name__ == "Amenity":
-                self.amenity_ids.append(amenity_obj.id)
