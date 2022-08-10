17544bdbb7afa768854a019bfcd02351dbaf0081
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-10 04:19:28 +0000

    fix

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from models.place import place_amenity
-from sqlalchemy import Column, String, ForeignKey
+"""Amenity class for AirBnb project"""
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
+    """Amenity class that creates amenities table"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
+    place_amenities = relationship("Place", secondary=place_amenity,
                                    viewonly=True)
