376d188746a0e4c2b148dbcc431380283e69ad62
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-25 15:50:07 +0000

    add 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#Write a Bash script that sets up your web servers for the deployment of web_static. It must:
+sudo apt update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch -p /data/web_static/releases/test/index.html
+sudo echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" >  /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
+sudo service nginx restart
