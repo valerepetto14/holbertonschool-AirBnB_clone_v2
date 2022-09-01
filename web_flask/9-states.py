c7c52f596fa1b05c1505872a810ddaaefaf8ad57
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-01 20:43:31 +0000

    add 9

---
+++ web_flask/9-states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""create app"""
+from flask import Flask, request, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def tear_down(self):
+    """
+    After each request you must remove the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def states_list():
+    """Import data from storage"""
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route("states/<id>", strict_slashes=False)
+def states_cities(id):
+    """Import data from storage"""
+    states = storage.all(State).values()
+    state = states[id]
+    return render_template("9-states.html", state=state)
+
+if __name__ == '__main__':
+    """app run"""
+    app.run(host='0.0.0.0', port=5000)
