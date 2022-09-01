88e9672c7ecc962be1f8530c2745f04df23f765b
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-01 19:56:45 +0000

    changes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,55 +5,6 @@ from models import storage
 from models.state import State
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def hello():
-    """def hello"""
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """def hbnb"""
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """def hbnb"""
-    return '{}'.format(text.replace("_"," "))
-
-
-@app.route('/python/<text>', strict_slashes=False)
-@app.route('/python/')
-def python(text="is cool"):
-    """def hbnb"""
-    return '{}'.format(text.replace("_"," "))
-
-
-@app.route('/number/<n>', strict_slashes=False)
-def number(n):
-    """def number"""
-    if n.isdigit() == True:
-        return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<n>', strict_slashes=False)
-def number_template(n):
-    """def number"""
-    if n.isdigit() == True:
-        return render_template("5-number.html", number=n)
-
-
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """def number_odd_or_even"""
-    if n.isdigit() == True:
-        if int(n) % 2 == 0:
-            return render_template("6-number_odd_or_even.html",number=n, type="even")
-        return render_template("6-number_odd_or_even.html", number=n, type="odd")
-
-
 @app.teardown_appcontext
 def tear_down(self):
     """
