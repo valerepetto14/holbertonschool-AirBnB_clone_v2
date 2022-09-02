29d3e3a74b04dd0deabc1601b442081dd44409b8
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-02 02:06:27 +0000

    fix amenities

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -18,7 +18,7 @@ def tear_down(self):
 def states_list():
     """Import data from storage"""
     states = storage.all(State).values()
-    return render_template("10-hbnb_filters.html", states=states, amenetis=states.amenities)
+    return render_template("10-hbnb_filters.html", states=states)
 
 
 if __name__ == '__main__':
