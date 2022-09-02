3a393bf64955ecc6cec32bfd6c6cb14b825b16f6
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-02 02:56:34 +0000

    more test

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -21,8 +21,8 @@ def states_list():
     states = storage.all(State).values()
     for state in states:
         for city in state.cities:
+            print(city)
             for place in city.places:
-                print(place)
     return render_template("10-hbnb_filters.html", states=states)
 
 
