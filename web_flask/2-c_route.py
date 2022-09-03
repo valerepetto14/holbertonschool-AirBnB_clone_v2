cc109c75914cd7c02efad4c374f1b7121445d2c8
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-03 17:44:24 +0000

    fix 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,8 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def display(text):
     """def display"""
-    return "C {}".format(text.replace("_"," "))
-
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
