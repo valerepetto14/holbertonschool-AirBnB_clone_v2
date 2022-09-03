47c6fef9eacb1f6da38018fe31feef40e7f0accc
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-03 17:51:33 +0000

    fix 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,8 +39,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """def number"""
-    if n.isdigit() is True:
-        return render_template("5-number.html", number=n)
+    return render_template("5-number.html", number=n)
 
 
 if __name__ == '__main__':
