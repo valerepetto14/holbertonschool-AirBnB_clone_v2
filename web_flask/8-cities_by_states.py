2e222554de01784fd6ea8e9430103dd74f800bf1
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-03 18:11:14 +0000

    fix 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@ from models import storage
 from models.state import State
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def tear_down(self):
     """
@@ -25,6 +26,7 @@ def states_cities():
     """Import data from storage"""
     states = storage.all(State).values()
     return render_template("8-cities_by_states.html", states=states)
+
 
 if __name__ == '__main__':
     """app run"""
