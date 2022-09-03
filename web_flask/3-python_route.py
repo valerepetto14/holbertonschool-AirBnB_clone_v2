e651cda62599e019d27f8606b16f56cc1e0b6bc8
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-03 17:46:36 +0000

    fix 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,27 +7,28 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """def hello"""
+    """def hello that return helo HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """def hbnb"""
+    """def hbnb that return HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """def hbnb"""
+    """def c that return the variable text"""
     return f'C {text.replace("_"," ")}'
 
 
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python', strict_slashes=False)
 def python(text="is cool"):
-    """def hbnb"""
-    return f'Python {text.replace("_"," ")}'
+    """def python"""
+    return 'Python {}'.format(text.replace("_", " "))
+
 
 if __name__ == '__main__':
     """app run"""
